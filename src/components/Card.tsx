2b3c6d93a27c043e2fcdd85e75c316d8694e5f86
akash23-ai <akashbhardwaj2024@gmail.com>
2024-08-08 22:35:12 +0530

    Fixed some bugs

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,17 +1,18 @@
 import { getBalanceAndAirdrop } from "@/app/server/action";
 import { LAMPORT } from "@/lib/constants";
 import Image from "next/image";
-import { useState } from "react";
+import { Dispatch, SetStateAction, useRef, useState } from "react";
 
 type BalanceOrAirdrop = "balance" | "Airdrop";
 
 export function Card() {
   const [balanceOrAirdrop, setBalanceOrAirdrop] =
     useState<BalanceOrAirdrop>("balance");
-  const [publicKey, setPublicKey] = useState();
-  const [balance, setBalance] = useState(null);
-  const [token, setToken] = useState(null);
+  const [publicKey, setPublicKey] = useState<string>();
+  const [balance, setBalance] = useState<number | null>(null);
+  const [token, setToken] = useState<string | null>("shhd");
   const [loading, setLoading] = useState<Boolean>(false);
+  const [model, setModel] = useState<Boolean>(false);
 
   const sendBalanceOrAirdropRequest = async (type: BalanceOrAirdrop) => {
     if (!publicKey) {
@@ -25,11 +26,11 @@ export function Card() {
         publicKey,
         method: "getBalance",
       });
-      if(response === "Error"){
+      if (response === "Error") {
         alert("You have given wrong publick key");
-        return
+        return;
       }
-      setBalance((response.result.value));
+      setBalance(response.result.value);
     } else if (type === "Airdrop") {
       setLoading(true);
       const response = await getBalanceAndAirdrop({
@@ -38,10 +39,12 @@ export function Card() {
         method: "requestAirdrop",
       });
 
-      if(response === "Error"){
-        alert("You have requested for too many Airdrop. Try again after 24 hours");
+      if (response === "Error") {
+        alert(
+          "You have requested for too many Airdrop. Try again after 24 hours"
+        );
         setLoading(false);
-        return
+        return;
       }
       setToken(response.result);
       setLoading(false);
@@ -50,7 +53,11 @@ export function Card() {
 
   return (
     <div className="absolute flex justify-center w-full top-48">
-      <div className="flex flex-col justify-center items-center w-[35%] rounded-md border border-white px-4 py-8">
+      <div
+        className={`flex flex-col justify-center items-center w-[35%] ${
+          model ? "blur-sm" : ""
+        } rounded-md border border-white px-4 py-8`}
+      >
         <h1 className="text-2xl font-semibold mb-4">
           Check Balance and Airdrop SOLs
         </h1>
@@ -114,7 +121,7 @@ export function Card() {
               {balance && (
                 <div className="w-full text-[#2596be]">
                   <p className="flex justify-center font-bold items-center text-xl">
-                    Your Balance is {balance/LAMPORT} SOL
+                    Your Balance is {balance / LAMPORT} SOL
                   </p>
                 </div>
               )}
@@ -142,11 +149,11 @@ export function Card() {
 
               <div className="w-full mb-6">
                 <button
-                  className="p-2 bg-transparent border border-white w-full rounded-lg hover:cursor-pointer flex justify-center items-center"
+                  className="p-2 bg-transparent border border-white w-full rounded-lg hover:cursor-pointer flex justify-center items-center mb-4"
                   onClick={() => sendBalanceOrAirdropRequest("Airdrop")}
                 >
                   {loading ? (
-                    <div className="animate-spin h-6 w-6 bg-white rounded-md" ></div>
+                    <div className="animate-spin h-6 w-6 bg-white rounded-md"></div>
                   ) : (
                     <>
                       <span className="pr-2">Airdrop</span>
@@ -161,9 +168,109 @@ export function Card() {
                     </>
                   )}
                 </button>
+
+                {token && (
+                  <button
+                    className="p-2 bg-transparent border border-white w-full rounded-lg hover:cursor-pointer flex justify-center items-center"
+                    onClick={() => setModel(true)}
+                  >
+                    <span className="pr-2">Check the transaction</span>
+                    <Image
+                      src={"/airdrop.png"}
+                      width={40}
+                      height={40}
+                      style={{ width: "auto", height: "auto" }}
+                      alt="airdrop-image"
+                      className="contrast-75"
+                    />
+                  </button>
+                )}
               </div>
             </div>
           )}
+        </div>
+      </div>
+
+      {model && <Model setModel={setModel} />}
+    </div>
+  );
+}
+
+function Model({ setModel }: { setModel: Dispatch<SetStateAction<Boolean>> }) {
+  const [copied, setCopied] = useState<Boolean>(false);
+  const inputRef = useRef(null);
+
+  return (
+    <div className="absolute flex justify-center items-center w-full h-full">
+      <div className="relative w-[30%] border-2 border-orange-700 p-4 shadow-md rounded-md">
+        <h1 className="text-sm mb-4">
+          Any one with this link will be able to see this transaction
+        </h1>
+        <div className="absolute flex w-full h-full top-[0.8rem] justify-end right-3" onClick={() => setModel(false)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="white"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="size-6 hover:animate-bounce"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M6 18 18 6M6 6l12 12"
+            />
+          </svg>
+        </div>
+        <div className="flex justify-center items-center gap-4 mb-4">
+          <input
+            type="text"
+            value={"hgd"}
+            ref={inputRef}
+            disabled
+            className="w-10/12 bg-transparent border-2 border-blue-700/80 px-2 py-2 rounded-md"
+          />
+          <div
+            className={`bg-gray-100 rounded-md ${!copied ? "p-1" : "p-2"}`}
+            onClick={async () => {
+              //@ts-ignore
+              const inputText = inputRef.current?.value;
+              await navigator.clipboard.writeText(inputText);
+              setCopied(true);
+              setTimeout(() => {
+                setCopied(false);
+              }, 5000);
+            }}
+          >
+            {!copied ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="black"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="size-7"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75"
+                />
+              </svg>
+            ) : (
+              <div className="text-xs font-semibold text-gray-800/80 hover:cursor-pointer">
+                Copied
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="flex justify-center w-full">
+          <button
+            className="px-4 py-2 w-4/5 bg-transparent border border-white rounded-lg"
+            onClick={() => {}}
+          >
+            Link
+          </button>
         </div>
       </div>
     </div>
