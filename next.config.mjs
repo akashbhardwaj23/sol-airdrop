b84a9ce442f4d615cc4e3398cb9a36fe981a3413
akash23-ai <akashbhardwaj2024@gmail.com>
2024-12-18 18:14:00 +0530

    solved solana image issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+   images : {
+    remotePatterns : [
+        {
+            hostname : 'solana.com',
+            protocol : 'https',
+            
+        }
+    ]
+   }    
+};
 
 export default nextConfig;
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,7 +5,7 @@ export function Logo(){
     return (
         <div className="absolute flex justify-center top-16 w-full mb-8 ">
         <Image
-          src={"https://solana.com/_next/static/media/logotype.e4df684f.svg"}
+          src={"https://solana.com/favicon.png"}
           width={600}
           height={600}
           style={{width : "auto", height : "auto"}}
